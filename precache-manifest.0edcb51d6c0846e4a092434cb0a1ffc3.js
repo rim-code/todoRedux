97b1ed00343a23235c6ca0e409f130baba24ed96
rim-code <friouirim@gmail.com>
2019-12-16 11:07:56 +0100

    Updates

---
+++ precache-manifest.0edcb51d6c0846e4a092434cb0a1ffc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f661f325396966add6a9621adff20d6",
+    "url": "/todoRedux/index.html"
+  },
+  {
+    "revision": "e567f13691d7933f9125",
+    "url": "/todoRedux/static/css/main.bd5ea8ed.chunk.css"
+  },
+  {
+    "revision": "9c9f5324d363d1e7d521",
+    "url": "/todoRedux/static/js/2.9791878d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todoRedux/static/js/2.9791878d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e567f13691d7933f9125",
+    "url": "/todoRedux/static/js/main.4bd8c657.chunk.js"
+  },
+  {
+    "revision": "140a4b0e590e6f7bc7f7",
+    "url": "/todoRedux/static/js/runtime-main.dec75326.js"
+  }
+]);
